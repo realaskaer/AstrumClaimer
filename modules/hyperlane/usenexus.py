e8704035da4287cb4cffdd3335a52e3a1a5f2060
user <realaskaer@gmail.com>
2025-04-22 17:51:41 +0300

    v1.7.9

--- modules/hyperlane/usenexus.py
+++ modules/hyperlane/usenexus.py
@@ -2,7 +2,9 @@ from eth_abi import abi
 from web3.contract import AsyncContract
 from config import TOKENS_PER_CHAIN
 from config import USENEXUS_ABI
+from dev import Settings
 from modules import Logger, EVMClient
+from modules.interfaces import SoftwareException
 from utils.tools import helper, gas_checker
 
 
@@ -25,18 +27,35 @@ class UseNexus(Logger):
             TOKENS_PER_CHAIN[self.client.network.name]['HYPER'], USENEXUS_ABI['router']
         )
 
-        bridge_gas_fee = await bridge_contract.functions.quoteGasPayment(
-            dest_chain_int
-        ).call()
-
+        bridge_gas_fee = await bridge_contract.functions.quoteGasPayment(dest_chain_int).call()
         int_address = self.client.w3.to_int(hexstr=self.client.address)
         dst_address = abi.encode(['uint256'], [int_address])
 
-        transaction = await bridge_contract.functions.transferRemote(
-            dest_chain_int,
-            dst_address,
-            amount_in_wei
-        ).build_transaction(await self.client.prepare_transaction(value=bridge_gas_fee))
+        try:
+            transaction = await bridge_contract.functions.transferRemote(
+                dest_chain_int,
+                dst_address,
+                amount_in_wei
+            ).build_transaction(await self.client.prepare_transaction(value=bridge_gas_fee))
+        except Exception as error:
+            if 'gas required exceeds' in str(error) or 'insufficient funds' in str(error):
+                from modules.custom_modules import Custom
+
+                cex_chain_id = {
+                    'Arbitrum': 2,
+                    'Optimism': 3,
+                    'Base': 6,
+                    'BNB Chain': 8,
+                }[self.client.network.name]
+
+                Settings.OKX_WITHDRAW_DATA = [
+                    [cex_chain_id, (0.00035, 0.0004)],
+                ]
+
+                await Custom(self.client).smart_cex_withdraw(dapp_id=Settings.HYPERLANE_CEX_USE)
+                raise SoftwareException('Exception for retry...')
+            else:
+                raise error
 
         old_balance_data_on_dst = await self.client.wait_for_receiving(
             chain_to_name='BNB Chain', token_name="HYPER", check_balance_on_dst=True
